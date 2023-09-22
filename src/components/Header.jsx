edd01d3e98f657dd12d38719fe7ec03f016b7c7d
syed mosa <syedmosa@syeds-MacBook-Pro.local>
2023-09-22 13:17:51 +0500

    second Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import Headerimg1 from '/discord.png'
 import Headerimg2 from '/twitter.png'
+import MobNav from './MobNav'
+
 function Header() {
+
+  const[vale,setVal]=useState('flase')
+
+function handleClick(){
+if(vale=="flase"){
+setVal('true')
+}
+else{
+  setVal('flase')
+}
+
+
+}
+
+  
   return <>
+  {vale=="true" &&  <MobNav/> }
+
+
+
+
+
   <div className="headerContainer">
     <h1 > <a href="#hero">COINSYY</a></h1>
-    <ul>
+   
+    <ul >
       <li><a href="#hero">Home</a></li>
       <li><a href="#market">market</a></li>
       <li><a href="#chooseUS">Choose US</a></li>
       <li><a href="#Footer">Join</a></li>
+
+ 
+
+
 
 
 
 
 
     </ul>
+
+ 
+ <button onClick={handleClick}><i className="fa-solid fa-bars" style={{color:"#fff"}}></i></button>
+
 
     <div className="headIcons">
       <img src={Headerimg1} alt="" />
---
+++ src/components/MobNav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Mob.css'
+function MobNav() {
+  return (
+    <div id='f'>  
+    <ul >
+    <li><a href="#hero">Home</a></li>
+    <li><a href="#market">market</a></li>
+    <li><a href="#chooseUS">Choose US</a></li>
+    <li><a href="#Footer">Join</a></li>
+
+
+
+
+
+
+
+
+
+  </ul></div>
+  )
+}
+
+export default MobNav
